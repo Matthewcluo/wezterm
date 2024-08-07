935bd1b8422b9c3e21aa48ae0f3b8b85e1e80106
Matthew Luo <mattcluo@amazon.com>
2024-08-07 12:01:32 -0700

    wezterm.lua: change tab bar settings

--- wezterm.lua
+++ wezterm.lua
@@ -10,10 +10,27 @@ config.color_scheme = "tokyonight_night"
 
 config.font_size = 16
 
-config.enable_tab_bar = false
+config.enable_tab_bar = true
+config.hide_tab_bar_if_only_one_tab = true
+config.use_fancy_tab_bar = false
 
 config.initial_cols = 144
 config.initial_rows = 36
+
+-- Keybinds for splitting panes
+-- local act = wezterm.action
+-- config.keys = {
+-- 	{
+-- 		key = "-",
+-- 		mods = "SHIFT|ALT",
+-- 		action = act.SplitVertical({ domain = "CurrentPaneDomain" }),
+-- 	},
+-- 	{
+-- 		key = "=",
+-- 		mods = "SHIFT|ALT",
+-- 		action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+-- 	},
+-- }
 
 -- and finally, return the configuration to wezterm
 return config
